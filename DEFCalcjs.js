59209402274695a3f13d37e3336bf4432f72544b
BreannW <breannwasson@gmail.com>
2016-02-29 11:10:11 -0500

    Update DEFCalcjs.js

--- DEFCalcjs.js
+++ DEFCalcjs.js
@@ -2,27 +2,7 @@ document.getElementById('DEFsubmitbtn').addEventListener("click", CalcDef);
 
 	function CalcDef(event) {
 	        event.preventDefault();
-	       /* if (document.getElementById('NumberOfTrucks').value === '') {
-	            document.getElementById("NOT").innerHTML =
-	                'Number of Trucks Requiring DEF:  <span style="color:red">*</span>';
-	            document.getElementById('NumberOfTrucks').focus();
-				
-	        } 
-			
-			
-			if  (document.getElementById('MilesPerVeh').value === '') {
-	            document.getElementById('AMPV').innerHTML =
-	                'Annual Mileage per Vehicle<span style="color:red">*</span>';
-	            document.getElementById('MilesPerVeh').focus();
-				
-	        }
-			
-	        if (document.getElementById('AverageMPG').value === '') {
-	            document.getElementById("AMPG").innerHTML =
-	                'Average Miles per Gallon <span style="color:red">*</span>';
-	            document.getElementById('AverageMPG').focus();
-	        } 
-			*/
+	  
 			
 			
 	            var NumberOfTrucks = document.getElementById('NumberOfTrucks').value;
@@ -140,24 +120,12 @@ document.getElementById('DEFsubmitbtn').addEventListener("click", CalcDef);
 	                '<br>Send your OTR drivers with a bottle of DEF! <a href="mailto:DEFSolutions@atlasoil.com">Ask about our pre-packaged DEF goods today</a>!';
 	        }
 	    }
-	    /* function SizedTank() {
-if (DEFUsedAnnually >=0 && DEFUsedAnnually <= 2560) { document.getElementById("tankSize").innerHTML = "55 Gal Drum";
-	} else if (DEFUsedAnnually >= 2561 && DEFUsedAnnually <= 6400) { document.getElementById("tankSize").innerHTML = "257 Gallon Tote";
-	} else if (DEFUsedAnnually >= 6401 && DEFUsedAnnually <= 19200) { document.getElementById("tankSize").innerHTML = "1-2k Gallon Mini";
-	} else if (DEFUsedAnnually >= 19201 && DEFUsedAnnually <= 38400) { document.getElementById("tankSize").innerHTML = "3-4k Gallon Mini";
-	} else if (DEFUsedAnnually >=38401 && DEFUsedAnnually <=76800){ document.getElementById("tankSize").innerHTML = "5K UST";
-	} else { document.getElementById("tank").innerHTML = "Please Contact a Sales Representative"; } 
-	
-		
-		
-}*/
+
 	    /* IE11 Fix for SP2010 */
 	if (typeof(UserAgentInfo) != 'undefined' && !window.addEventListener) {
 	    UserAgentInfo.strBrowser = 1;
 	}
-	/* if (typeof console == "undefined") {
-    this.console = { log: function (msg) { alert(msg); } };
-} */
+
 	 // IE9 fix
 	if (!window.console) {
 	    var console = {
